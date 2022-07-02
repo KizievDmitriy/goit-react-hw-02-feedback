b3070aed4125f46a5cd3a11eb1de56622b31c62d
KizievDmitriy <kizievdmitrii@gmail.com>
2022-07-02 17:29:21 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,48 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Notification from './Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  hundleIncrement = option => {
+    this.setState(preState => ({ [option]: preState[option] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const obj = this.state;
+    return Object.values(obj).reduce((a, b, c) => a + b + c, -3);
+  };
+
+  countPositiveFeedbackPercentage = (a, b) => {
+    return Math.round((a / b) * 100) + '%';
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.hundleIncrement}
+        />
+        {this.countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </>
+    );
+  }
+}
